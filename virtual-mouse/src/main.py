9da5b9ea07a75991948d31890ed82d0c945041ab
Asalparvaz <spasalparvaz@gmail.com>
2026-02-25 16:54:50 +0330

    virtual-mouse: add cursor smoothening and better frame cropping

--- virtual-mouse/src/main.py
+++ virtual-mouse/src/main.py
@@ -1,19 +1,25 @@
 import cv2
 import numpy as np
-import time
 import autopy
 
 import hand_tracker_module as htm
 
 detector = htm.HandDetector(maxHands=1)
 
-wscreen, hscreen = autopy.screen.size()
-wcam, hcam = 640, 480
-frameR = 100 # Frame Reduction
+screen_width, screen_height = autopy.screen.size()
+camera_width, camera_height = 640, 480
+horizontal_frame_crop = 100 # Frame Reduction
+top_frame_crop = 50
+bottom_frame_crop = 150
+
+smoothening = 3
+ploc_x, ploc_y = 0, 0
+cloc_x, cloc_y = 0, 0
+
 
 cap = cv2.VideoCapture(1)
-cap.set(3, wcam)
-cap.set(4, hcam)
+cap.set(3, camera_width)
+cap.set(4, camera_height)
 
 while True:
     success, img = cap.read()
@@ -30,16 +36,20 @@ while True:
 
         fingers = detector.fingers_up()
 
-        cv2.rectangle(img, (frameR, frameR), (wcam - frameR, hcam - frameR), (0, 0, 255), 2)
+        cv2.rectangle(img, (horizontal_frame_crop, top_frame_crop), (camera_width - horizontal_frame_crop, camera_height - bottom_frame_crop), (0, 0, 255), 2)
 
         if fingers[1] and not fingers[2]:
             # moving mode
 
-            x3 = np.interp(x1, (frameR, wcam-frameR), (0, wscreen))
-            y3 = np.interp(y1, (frameR, hcam-frameR), (0, hscreen))
+            x3 = np.interp(x1, (horizontal_frame_crop, camera_width - horizontal_frame_crop), (0, screen_width))
+            y3 = np.interp(y1, (top_frame_crop, camera_height - bottom_frame_crop), (0, screen_height))
 
-            autopy.mouse.move(x3, y3)
+            cloc_x = ploc_x + (x3 - ploc_x) / smoothening
+            cloc_y = ploc_y + (y3 - ploc_y) / smoothening
+
+            autopy.mouse.move(cloc_x, cloc_y)
             cv2.circle(img, (x1,y1), 15, (0, 0, 255), cv2.FILLED)
+            ploc_x, ploc_y = cloc_x, cloc_y
 
 
         if fingers[1] and fingers[2]:
@@ -47,7 +57,7 @@ while True:
 
             length, img, line_info = detector.find_distance(8, 12, img)
 
-            if length < 40:
+            if length < 30:
                 cv2.circle(img, (line_info[4], line_info[5]), 15, (0, 255, 0), cv2.FILLED)
                 autopy.mouse.click()
 
