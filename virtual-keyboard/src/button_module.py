f21081f8dde23f5143364b657aa949187809c721
Asalparvaz <spasalparvaz@gmail.com>
2026-03-15 15:16:29 +0330

    virtual-keyboard: initial button test and module

---
+++ virtual-keyboard/src/button_module.py
@@ -0,0 +1,27 @@
+import cv2
+
+class Button():
+    def __init__(self, pos, width, height, value):
+        self.pos = pos
+        self.width = width
+        self.height = height
+        self.value = value
+
+    def draw(self, img):
+        cv2.rectangle(img, self.pos, (self.pos[0]+self.width, self.pos[1]+self.height),
+                      (213, 213, 213), cv2.FILLED)
+        cv2.rectangle(img, self.pos, (self.pos[0]+self.width, self.pos[1]+self.height),
+                      (50, 50, 50), 3)
+        cv2.putText(img, self.value, (int(self.pos[0] + self.width / 3.5), int(self.pos[1]+ self.height / 1.4)), cv2.FONT_HERSHEY_SIMPLEX,
+                    2, (50, 50, 50), 2)
+
+    def check_clicked(self, x, y, img):
+        if self.pos[0] < x < self.pos[0] + self.width and self.pos[1] < y < self.pos[1] + self.height:
+            cv2.rectangle(img, self.pos, (self.pos[0] + self.width, self.pos[1] + self.height),
+                          (150, 100, 255), cv2.FILLED)
+            cv2.rectangle(img, self.pos, (self.pos[0] + self.width, self.pos[1] + self.height),
+                          (50, 50, 50), 3)
+            cv2.putText(img, self.value, (self.pos[0] + 32, self.pos[1] + 70), cv2.FONT_HERSHEY_SIMPLEX,
+                        2, (50, 50, 50), 2)
+            return True
+        return False
--- virtual-keyboard/src/main.py
+++ virtual-keyboard/src/main.py
@@ -1,11 +1,14 @@
 import cv2
 import hand_tracker_module as htm
+import button_module as btn
 
 cap = cv2.VideoCapture(1)
 cap.set(3, 1280) #width
 cap.set(4, 720) #height
 
 detector = htm.HandDetector(detectionCon=0.8)
+
+button_test = btn.Button((100, 100),100, 100, 'Q')
 
 while True:
     success, img = cap.read()
@@ -15,6 +18,7 @@ while True:
     img = detector.find_hands(img)
     lmList = detector.find_position(img)
 
+    button_test.draw(img)
 
     cv2.imshow("Virtual Keyboard", img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
