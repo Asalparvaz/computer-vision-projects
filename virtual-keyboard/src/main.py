9cb5d0807aeb4757e76e03274965342a1c173a34
Asalparvaz <spasalparvaz@gmail.com>
2026-03-15 15:05:29 +0330

    virtual-keyboard: initial hand tracking and module

--- virtual-keyboard/src/main.py
+++ virtual-keyboard/src/main.py
@@ -1,1 +1,21 @@
-print("HI!:)")
+import cv2
+import hand_tracker_module as htm
+
+cap = cv2.VideoCapture(1)
+cap.set(3, 1280) #width
+cap.set(4, 720) #height
+
+detector = htm.HandDetector(detectionCon=0.8)
+
+while True:
+    success, img = cap.read()
+    if not success:
+        break
+    img = cv2.flip(img, 1)
+    img = detector.find_hands(img)
+    lmList = detector.find_position(img)
+
+
+    cv2.imshow("Virtual Keyboard", img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
