6572b501a45d584997d177eaf92d4d908f6eb951
Asalparvaz <spasalparvaz@gmail.com>
2026-03-17 17:20:28 +0330

    virtual-keyboard: show capitalize keys having small letters in backend

--- virtual-keyboard/src/button_module.py
+++ virtual-keyboard/src/button_module.py
@@ -12,7 +12,7 @@ class Button():
                       (213, 213, 213), cv2.FILLED)
         cv2.rectangle(img, self.pos, (self.pos[0]+self.width, self.pos[1]+self.height),
                       (50, 50, 50), 2)
-        cv2.putText(img, self.value, (int(self.pos[0] + self.width / 3.9), int(self.pos[1]+ self.height / 1.4)), cv2.FONT_HERSHEY_SIMPLEX,
+        cv2.putText(img, self.value.capitalize(), (int(self.pos[0] + self.width / 3.9), int(self.pos[1]+ self.height / 1.4)), cv2.FONT_HERSHEY_SIMPLEX,
                     0.8, (50, 50, 50), 2)
 
     def check_hover(self, x, y, img):
@@ -21,7 +21,7 @@ class Button():
                           (100, 100, 100), cv2.FILLED)
             cv2.rectangle(img, self.pos, (self.pos[0] + self.width, self.pos[1] + self.height),
                           (50, 50, 50), 2)
-            cv2.putText(img, self.value, (int(self.pos[0] + self.width / 3.9), int(self.pos[1] + self.height / 1.4)),
+            cv2.putText(img, self.value.capitalize(), (int(self.pos[0] + self.width / 3.9), int(self.pos[1] + self.height / 1.4)),
                         cv2.FONT_HERSHEY_SIMPLEX,
                         0.8, (50, 50, 50), 2)
             return True
@@ -33,7 +33,7 @@ class Button():
                           (0, 200, 0), cv2.FILLED)
             cv2.rectangle(img, self.pos, (self.pos[0] + self.width, self.pos[1] + self.height),
                           (50, 50, 50), 2)
-            cv2.putText(img, self.value, (int(self.pos[0] + self.width / 3.9), int(self.pos[1] + self.height / 1.4)),
+            cv2.putText(img, self.value.capitalize(), (int(self.pos[0] + self.width / 3.9), int(self.pos[1] + self.height / 1.4)),
                         cv2.FONT_HERSHEY_SIMPLEX,
                         0.8, (50, 50, 50), 2)
             return True
--- virtual-keyboard/src/main.py
+++ virtual-keyboard/src/main.py
@@ -12,15 +12,15 @@ delay_counter = 0
 button_list = []
 
 english_keyboard_value = [['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '='],
-                          ['Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '[', ']'],
-                          ['Cap', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', '\'', 'Ent'],
-                          ['Ctrl', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', '/']]
+                          ['tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']'],
+                          ['cap', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', 'ent'],
+                          ['ctrl', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/']]
 
 for y in range(len(english_keyboard_value)):
     for x, value in enumerate(english_keyboard_value[y]):
         button_list.append(btn.Button((80 * x + 130, 80 * y + 20), value))
-button_list.append(btn.Button((80 * 11 + 130, 80 * 3 + 20), 'Shift', 150))
-button_list.append(btn.Button((130, 80 * 4 + 20), 'Alt'))
+button_list.append(btn.Button((80 * 11 + 130, 80 * 3 + 20), 'shift', 150))
+button_list.append(btn.Button((130, 80 * 4 + 20), 'alt'))
 button_list.append(btn.Button((210, 80 * 4 + 20), ' ', 70 * 4 + 30, 70))
 
 while True:
